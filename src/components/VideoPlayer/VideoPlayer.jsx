72a72a1faf6853673c4fe42757dbddc7dbe0b05b
kevinreber <kevinreber1@gmail.com>
2021-01-24 15:36:59 -0800

    added YT Iframe API script instead of YT component

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import YouTube from 'react-youtube';
 
 // MUI
@@ -39,6 +39,18 @@ import {
  * player.getVideoData()  => @returns {author: string; title: string; video_id: string; video_quality: string} Object containing video data.
  */
 
+/**
+ * Player Status:
+ * -1 = unstarted
+ * 0 = ended
+ * 1 = playing
+ * 2 = paused
+ * 3 = buffering
+ * 5 = cued
+ */
+
+// * Variable to control our YT Player
+let player;
 // ! NOTE: Avoided using typescript b/c opts passed into YouTube component gives too many errors
 const VideoPlayer = ({ curVideo, addVideoToList }) => {
 	const [url, setUrl] = useState('https://www.youtube.com/watch?v=OHviieMFY0c');
@@ -47,17 +59,49 @@ const VideoPlayer = ({ curVideo, addVideoToList }) => {
 		setUrl(e.target.value);
 	};
 
-	const opts = {
-		height: '480',
-		width: '854',
-		playerVars: {
-			// https://developers.google.com/youtube/player_parameters
-			autoplay: 1,
-		},
+	// Create player
+	const loadVideo = () => {
+		if (!player) {
+			player = new window.YT.Player('player', {
+				videoId: curVideo,
+				height: '480',
+				width: '854',
+				playerVars: {
+					// https://developers.google.com/youtube/player_parameters
+					autoplay: 1,
+				},
+				events: {
+					onReady: onPlayerReady,
+					onStateChange: handleStateChange,
+					onPlay: handlePlayOnPlayer,
+					onPause: handlePauseOnPlayer,
+				},
+			});
+		} else {
+			player.loadVideoById(curVideo);
+			console.log('loaded', curVideo, player);
+		}
 	};
 
+	// Load YT IFrame Player script into html
+	useEffect(() => {
+		if (curVideo !== null) {
+			if (!window.YT) {
+				const tag = document.createElement('script');
+				tag.src = 'https://www.youtube.com/iframe_api';
+
+				window.onYouTubeIframeAPIReady = loadVideo;
+
+				const firstScriptTag = document.getElementsByTagName('script')[0];
+				firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			} else loadVideo();
+		} else if (!curVideo && player) {
+			player.clearVideo();
+		}
+	}, [curVideo, player, loadVideo]);
+
 	/** Pauses current video on ready to avoid auto-play and logs video data */
-	const _onReady = (e) => {
+	const onPlayerReady = (e) => {
 		// access to player in all event handlers via event.target
 		e.target.pauseVideo();
 		console.log(e.target.getVideoData());
@@ -68,33 +112,59 @@ const VideoPlayer = ({ curVideo, addVideoToList }) => {
 		setUrl('');
 	};
 
-	const handlePlay = (e) => {
+	// Event Handlers for Player ------------------------
+	const handlePlayOnPlayer = (e) => {
 		console.log(e.target.getCurrentTime());
-		console.log(e.target.getDuration());
+		// console.log(e.target.getDuration());
+		console.log('play', YouTube.PlayerState);
+	};
+	const handlePauseOnPlayer = (e) => {
+		console.log(e.target.getCurrentTime());
+		// console.log(e.target.getDuration());
+		console.log('pause', player.getCurrentTime());
+	};
+	// -------------------------------------------------
+
+	const handlePlay = () => {
+		player.playVideo();
+		console.log('play', player.getCurrentTime());
 	};
 
-	const handlePause = (e) => {
-		console.log(e.target.getCurrentTime());
-		console.log(e.target.getDuration());
+	const handlePause = () => {
+		player.pauseVideo();
+		// console.log(e.target.getDuration());
+		console.log('pause', player.getCurrentTime());
 	};
 
+	const handleStateChange = (e) => {
+		console.log(e);
+		console.log(player);
+	};
+
+	// const player = (
+	// 	<YouTube
+	// 		videoId={curVideo}
+	// 		opts={opts}
+	// 		onReady={onPlayerReady}
+	// 		onPlay={handlePlay}
+	// 		onPause={handlePause}
+	// 		onStateChange={handleState}
+	// 	/>
+	// );
+	console.log('player', player);
 	return (
 		<div>
 			<input name="id" id="video-id" value={url} onChange={handleChange} />
 			<IconButton aria-label="add to queue" onClick={handleSubmit}>
 				<AddToQueue />
 			</IconButton>
-			<YouTube
-				videoId={curVideo}
-				opts={opts}
-				onReady={_onReady}
-				onPlay={handlePlay}
-				onPause={handlePause}
-			/>
-			<IconButton aria-label="play">
+			<div id="player">
+				<h3>No Video Found</h3>
+			</div>
+			<IconButton aria-label="play" onClick={handlePlay}>
 				<PlayArrowIcon />
 			</IconButton>
-			<IconButton aria-label="pause">
+			<IconButton aria-label="pause" onClick={handlePause}>
 				<PauseIcon />
 			</IconButton>
 			<IconButton aria-label="previous">
