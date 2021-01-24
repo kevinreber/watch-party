1d5cef094db7dbaa4bfff1479b8fd3870bfe786f
kevinreber <kevinreber1@gmail.com>
2021-01-23 18:03:45 -0800

    added notes for YT API Video Player

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -2,7 +2,32 @@ import React, { useState } from 'react';
 import YouTube from 'react-youtube';
 
 // * react-youtube: https://www.npmjs.com/package/react-youtube
+//<YouTube
+//	videoId={string} // defaults -> null
+//	id={string} // defaults -> null
+//	className={string} // defaults -> null
+//	containerClassName={string} // defaults -> ''
+//	opts={obj} // defaults -> {}
+//	onReady={func} // defaults -> noop
+//	onPlay={func} // defaults -> noop
+//	onPause={func} // defaults -> noop
+//	onEnd={func} // defaults -> noop
+//	onError={func} // defaults -> noop
+//	onStateChange={func} // defaults -> noop
+//	onPlaybackRateChange={func} // defaults -> noop
+//	onPlaybackQualityChange={func} // defaults -> noop
+///>
 // * get-youtube-id: https://www.npmjs.com/package/get-youtube-id
+
+/**
+ * * Getting Video Information:
+ * https://developers.google.com/youtube/iframe_api_reference#Retrieving_video_information
+ *
+ * methods:
+ * player.getCurrentTime() => @returns {float} Current time in video player.
+ * player.getDuration() => @returns {float} Total duration of video player.
+ * player.getVideoData()  => @returns {author: string; title: string; video_id: string; video_quality: string} Object containing video data.
+ */
 
 // ! NOTE: Avoided using typescript b/c opts passed into YouTube component gives too many errors
 const VideoPlayer = ({ curVideo, addVideoToList }) => {
@@ -21,14 +46,26 @@ const VideoPlayer = ({ curVideo, addVideoToList }) => {
 		},
 	};
 
+	/** Pauses current video on ready to avoid auto-play and logs video data */
 	const _onReady = (e) => {
 		// access to player in all event handlers via event.target
 		e.target.pauseVideo();
+		console.log(e.target.getVideoData());
 	};
 
 	const handleSubmit = () => {
 		addVideoToList(url);
 		setUrl('');
+	};
+
+	const handlePlay = (e) => {
+		console.log(e.target.getCurrentTime());
+		console.log(e.target.getDuration());
+	};
+
+	const handlePause = (e) => {
+		console.log(e.target.getCurrentTime());
+		console.log(e.target.getDuration());
 	};
 
 	return (
@@ -37,7 +74,13 @@ const VideoPlayer = ({ curVideo, addVideoToList }) => {
 			<button type="button" onClick={handleSubmit}>
 				Add to Queue
 			</button>
-			<YouTube videoId={curVideo} opts={opts} onReady={_onReady} />
+			<YouTube
+				videoId={curVideo}
+				opts={opts}
+				onReady={_onReady}
+				onPlay={handlePlay}
+				onPause={handlePause}
+			/>
 		</div>
 	);
 };
