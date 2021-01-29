f9c15fe80f6b2c3d94061eb0b4f0bb05ddc5ea0f
kevinreber <kevinreber1@gmail.com>
2021-01-28 17:50:47 -0800

    refactored code to start server

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@
 
 const express = require('express');
 const app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
 	socket.on('send-play', (data) => {
@@ -14,10 +14,4 @@ io.on('connection', (socket) => {
 	});
 });
 
-module.exports = app;
-
-// Access to XMLHttpRequest at 'http://localhost:3001/socket.io/?EIO=4&transport=polling&t=NTBWre_'
-// from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin'
-// header is present on the requested resource.
-
-// polling-xhr.js:202 GET http://localhost:3001/socket.io/?EIO=4&transport=polling&t=NTBWre_ net::ERR_FAILED
+module.exports = server;
--- server/server.js
+++ server/server.js
@@ -1,24 +1,8 @@
 /** server for watch party */
-// const app = require('./app');
+
+const server = require('./app');
 const PORT = 3001;
 
-// const express = require('express');
-// const app = express();
-// var server = require('http').createServer(app);
-
-const io = require('socket.io')(PORT);
-
-// const io = require('socket.io')(PORT);
-
-// app.listen(PORT, () => {
-// 	console.log(`Server starting on port ${PORT}`);
-// });
-
-io.on('connection', (socket) => {
-	socket.on('send-play', (data) => {
-		console.log('play', data);
-	});
-	socket.on('send-pause', (data) => {
-		console.log('pause', data);
-	});
+server.listen(PORT, () => {
+	console.log(`Server starting on port ${PORT}`);
 });
