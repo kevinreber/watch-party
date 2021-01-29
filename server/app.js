e3be626b5cce2c97922376e347399c2aabca560e
kevinreber <kevinreber1@gmail.com>
2021-01-28 22:51:31 -0800

    refactored event broadcast

--- server/app.js
+++ server/app.js
@@ -5,27 +5,11 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-/**
- * Player Status:
- * -1 = unstarted
- * 0 = ended
- * 1 = playing
- * 2 = paused
- * 3 = buffering
- * 5 = cued
- */
 io.on('connection', (socket) => {
 	socket.on('event', (data) => {
-		if (data.state === 'play') {
-			console.log('play', data);
-			socket.broadcast.emit('receive-event', data);
-		} else if (data.state === 'pause') {
-			console.log('pause', data);
-			socket.broadcast.emit('receive-event', data);
-		} else {
-			console.log('new timestamp', data);
-			socket.broadcast.emit('receive-event', data);
-		}
+		//  data.state : 'play' | 'pause' | 'seek'
+		console.log(data.state, data);
+		socket.broadcast.emit('receive-event', data);
 	});
 });
 
