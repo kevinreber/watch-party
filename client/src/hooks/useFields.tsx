1810f7df77a8e92d9d867d0799218da46cd6edea
kevinreber <kevinreber1@gmail.com>
2021-02-06 22:09:50 -0800

    added re-usable form hook

---
+++ client/src/hooks/useFields.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+/** useFields handles the state of the form data */
+function useFields(INITIAL_STATE: any) {
+	const [formData, setFormData] = useState(INITIAL_STATE);
+
+	/** Update state in formData */
+	const handleChange = (e: any) => {
+		const { name, value } = e.target;
+		setFormData((fData: any) => ({
+			...fData,
+			[name]: value,
+		}));
+	};
+
+	const resetFormData = () => setFormData(INITIAL_STATE);
+
+	return [formData, handleChange, resetFormData];
+}
+
+export default useFields;
