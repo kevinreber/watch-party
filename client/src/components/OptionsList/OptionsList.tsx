9d2c980d07dbc76a3940a2736adf1de31dbe6f3e
kevinreber <kevinreber1@gmail.com>
2021-02-23 22:37:21 -0800

    removed unused code

--- client/src/components/OptionsList/OptionsList.tsx
+++ client/src/components/OptionsList/OptionsList.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import ImageIcon from '@material-ui/icons/Image';
-import WorkIcon from '@material-ui/icons/Work';
-import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 interface OptionsTypes {
 	videoId: string;
 	channel: string;
@@ -43,9 +39,9 @@ const OptionsList = ({
 					</ListItem>
 				))
 			) : (
-				<li>
+				<ListItem>
 					<em>No Matches</em>
-				</li>
+				</ListItem>
 			)}
 		</List>
 	);
