996df60297330be33be9beb1573bb6493e145d0b
kevinreber <kevinreber1@gmail.com>
2022-07-04 21:46:56 -0700

    added logs

--- server/utils/youtube.js
+++ server/utils/youtube.js
@@ -1,53 +1,59 @@
-const config = require('../config');
+const config = require("../config");
 // https://www.npmjs.com/package/youtube-api
-const Youtube = require('youtube-api');
+const Youtube = require("youtube-api");
 
 if (config.YOUTUBE_API_KEY) {
-	Youtube.authenticate({
-		type: 'key',
-		key: config.YOUTUBE_API_KEY,
-	});
+  Youtube.authenticate({
+    type: "key",
+    key: config.YOUTUBE_API_KEY,
+  });
 }
 
 // Re-formats data for each YouTube video
 const mapYoutubeSearchResult = (video) => {
-	return {
-		videoId: video.id.videoId,
-		channel: video.snippet.channelTitle,
-		description: video.snippet.description,
-		url: 'https://www.youtube.com/watch?v=' + video.id.videoId,
-		name: video.snippet.title,
-		img: video.snippet.thumbnails.default.url,
-	};
+  return {
+    videoId: video.id.videoId,
+    channel: video.snippet.channelTitle,
+    description: video.snippet.description,
+    url: "https://www.youtube.com/watch?v=" + video.id.videoId,
+    name: video.snippet.title,
+    img: video.snippet.thumbnails.default.url,
+  };
 };
 
 const searchYoutube = (query) => {
-	const options = { part: 'snippet', type: 'video', maxResults: 5, q: query };
-	return new Promise((resolve, reject) => {
-		Youtube.search.list(options, (err, data) => {
-			if (data && data.data.items) {
-				const response = data.data.items.map(mapYoutubeSearchResult);
-				resolve(response);
-			} else {
-				console.warn(data);
-				reject();
-			}
-		});
-	});
+  const options = { part: "snippet", type: "video", maxResults: 5, q: query };
+  return new Promise((resolve, reject) => {
+    Youtube.search.list(options, (err, data) => {
+      console.log("data is");
+      console.log(data);
+      console.log("error is");
+      console.log(err);
+      if (data && data.data.items) {
+        const response = data.data.items.map((item) =>
+          mapYoutubeSearchResult(item)
+        );
+        resolve(response);
+      } else {
+        console.warn(data);
+        reject();
+      }
+    });
+  });
 };
 
 const getYoutubeVideoID = (url) => {
-	// const idParts = YOUTUBE_VIDEO_ID_REGEX.exec(url);
-	if (!idParts) {
-		return;
-	}
+  // const idParts = YOUTUBE_VIDEO_ID_REGEX.exec(url);
+  if (!idParts) {
+    return;
+  }
 
-	const id = idParts[1];
-	if (!id) {
-		return;
-	}
+  const id = idParts[1];
+  if (!id) {
+    return;
+  }
 
-	return id;
+  return id;
 };
 
 module.exports = { searchYoutube };
